8f1cbea65926e15e96a676a5b6e284d1edf45ac3
Quanzheng Long <longer@uber.com>
2019-03-11 13:07:20 -0700

    Add more field to corrupted error log

--- common/persistence/historyV2Store.go
+++ common/persistence/historyV2Store.go
@@ -343,7 +343,7 @@ func (m *historyV2ManagerImpl) readHistoryBranch(byBatch bool, request *ReadHist
 			continue
 		}
 		if *fe.EventId != token.LastEventID+1 {
-			logger.Errorf("Corrupted incontinouous event batch, %v, %v, %v, %v, %v", *fe.Version, *le.Version, *fe.EventId, *le.EventId, el)
+			logger.Errorf("Corrupted incontinouous event batch, %v, %v, %v, %v, %v, %v", *fe.Version, *le.Version, *fe.EventId, *le.EventId, el, token.LastEventID)
 			return nil, nil, nil, 0, 0, &workflow.InternalServiceError{
 				Message: fmt.Sprintf("corrupted history event batch, eventID is not continouous"),
 			}
