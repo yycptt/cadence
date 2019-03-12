fc8f8f6fab62cd26f80d9624e67ebd65692f498a
Quanzheng Long <longer@uber.com>
2019-03-12 12:08:52 -0700

    add stack to error

--- common/persistence/historyV2Store.go
+++ common/persistence/historyV2Store.go
@@ -23,6 +23,8 @@ package persistence
 import (
 	"fmt"
 
+	"runtime/debug"
+
 	"github.com/pborman/uuid"
 	"github.com/uber-common/bark"
 	workflow "github.com/uber/cadence/.gen/go/shared"
@@ -343,7 +345,7 @@ func (m *historyV2ManagerImpl) readHistoryBranch(byBatch bool, request *ReadHist
 			continue
 		}
 		if *fe.EventId != token.LastEventID+1 {
-			logger.Errorf("Corrupted incontinouous event batch, %v, %v, %v, %v, %v, %v", *fe.Version, *le.Version, *fe.EventId, *le.EventId, el, token.LastEventID)
+			logger.Errorf("Corrupted incontinouous event batch, %v, %v, %v, %v, %v, %v, %v", *fe.Version, *le.Version, *fe.EventId, *le.EventId, el, token.LastEventID, debug.Stack())
 			return nil, nil, nil, 0, 0, &workflow.InternalServiceError{
 				Message: fmt.Sprintf("corrupted history event batch, eventID is not continouous"),
 			}
